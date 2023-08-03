d3eb8173623a2a49378f82b960f7c1cf0cd6841f
Rhuan Freire <rhuan.freire@rbmweb.com.br>
2023-08-03 10:28:09 -0300

    feat: CreatePost component

--- src/components/create-post/CreatePost.tsx
+++ src/components/create-post/CreatePost.tsx
@@ -1,22 +1,26 @@
-import { useRef} from "react";
+import { useRef } from "react";
+import styles from "./CreatePost.module.css";
 
-const CreatePost = (props:{onCreatePost: (title:string, message:string) => void}) => {
+const CreatePost = (props: {
+  onCreatePost: (title: string, message: string) => void;
+}) => {
   const titleRef = useRef<HTMLInputElement>(null);
-  const messageRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
 
   function clickHandler() {
-    if(titleRef.current?.value.length && messageRef.current?.value.length) {
+    if (titleRef.current?.value.length && messageRef.current?.value.length) {
       props.onCreatePost(titleRef.current.value, messageRef.current.value);
     }
   }
 
   return (
-    <div>
-      <input placeholder="Titulo" ref={titleRef}></input>
-      <input placeholder="Mensagem" ref={messageRef}></input>
-      <button onClick={clickHandler}>Enviar</button>
+    <div className={styles.container}>
+      <h2 className={styles.h2}>Compartilhe um Conhecimento</h2>
+      <input className={styles.input} type="text" placeholder="Título" ref={titleRef}></input>
+      <textarea className={styles["text-area"]} placeholder="Texto" ref={messageRef}></textarea>
+      <button className={styles.button} onClick={clickHandler}>Enviar</button>
     </div>
   );
-}
+};
 
 export default CreatePost;
