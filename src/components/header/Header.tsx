45caf590f73470fc573fdb7faeb40e6183acf4fd
Rhuan Freire <rhuan.freire@rbmweb.com.br>
2023-08-03 17:38:49 -0300

    fix: media

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ const Header = (props:{currentUser: string | undefined, onLogout: () => void}) =
     <div className={style["header-container"]}>
       <div className={style["logo-container"]}>
         <img className={style.icon} src={icon} />
-        <h1>Habilite+</h1>
+        <h1 className={style.logo}>Habilite+</h1>
       </div>
       <div className={style["logout-container"]}>
         <label className={style["email-label"]}>Bem vindo, {props.currentUser}</label>
