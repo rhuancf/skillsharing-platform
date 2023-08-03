267d1df906b8d3af9d1097eb05af8384043feeed
Rhuan Freire <rhuan.freire@rbmweb.com.br>
2023-08-03 16:00:48 -0300

    improvement: Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,17 @@ import style from './Header.module.css';
 import icon from '../../assets/icon2.png';
 
 
-const Header = () => {
+const Header = (props:{currentUser: string | undefined, onLogout: () => void}) => {
   return (
     <div className={style["header-container"]}>
-      <img className={style.icon} src={icon} />
-      <h1>Habilite+</h1>
+      <div className={style["logo-container"]}>
+        <img className={style.icon} src={icon} />
+        <h1>Habilite+</h1>
+      </div>
+      <div className={style["logout-container"]}>
+        <label className={style["email-label"]}>Bem vindo, {props.currentUser}</label>
+        <button onClick={props.onLogout} className={style["logout-button"]}>Desconectar</button>
+      </div>
     </div>
   );
 };
